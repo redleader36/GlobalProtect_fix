b359ad137f6f86767e72a910cd59aff1ca50fd93
muric <redleader36@gmail.com>
2023-01-05 14:16:18 -0600

    Update globalprotect_fix.ps1

--- globalprotect_fix.ps1
+++ globalprotect_fix.ps1
@@ -1,11 +1,1 @@
-$registryPath = "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings"
-$Name = "default-browser"
-$value = "YES"
-
-IF(!(Test-Path $registryPath)) {
-    New-Item -Path $registryPath -Force | Out-Null
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
-}
-ELSE {
-    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
-}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\Settings" -Name "default-browser" -Value "YES" -PropertyType String -Force | Out-Null
